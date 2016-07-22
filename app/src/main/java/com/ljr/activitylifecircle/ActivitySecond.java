9da2efd60b46857a5c44b1a049f7f87c17b7fc88
woshiljr <58037357@qq.com>
2016-07-22 10:59:33 +0800

    生命周期演示

---
+++ app/src/main/java/com/ljr/activitylifecircle/ActivitySecond.java
@@ -0,0 +1,10 @@
+package com.ljr.activitylifecircle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class ActivitySecond  extends AppCompatActivity{
+
+}
